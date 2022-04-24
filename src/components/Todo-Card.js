21fd51f75cc4e5674b7fa9c66c17808ffbb89a71
tmazyrko <tmazyrko@gmail.com>
2022-04-24 17:59:46 -0400

    Fix type coercion

--- src/components/Todo-Card.js
+++ src/components/Todo-Card.js
@@ -7,15 +7,30 @@ const ToDoCard = ({functions, task}) => {
         functions.moveTaskToNextColumn(task.id);
     };
 
+    const retractHandler = (e) => {
+        e.preventDefault();
+        functions.moveTaskToPreviousColumn(task.id);
+    }
+
     return (
-        <div className={`card`} key={task.id}>
+        <div className={`card mb-2`} key={task.id}>
             <div className="card-body align-items-center">
                 <h5 className="">{task.title}</h5>
                 <p className="">Type: {task.Type}</p>
                 <p className="">ID: {task.id}</p>
-                <button className="btn btn-primary" onClick={advanceHandler}>
-                    <i className="bi bi-check-lg"></i>
-                </button>
+
+                { task.status !== "Todo" ? (
+                    <button className="btn btn-primary" onClick={retractHandler}>
+                        <i className="bi bi-arrow-left" />
+                    </button>
+                ) : null }
+
+                { task.status !== "Done" ? (
+                    <button className="btn btn-primary ms-1" onClick={advanceHandler}>
+                        <i className="bi bi-arrow-right" />
+                    </button>
+                ) : null }
+
             </div>
         </div>
     );
